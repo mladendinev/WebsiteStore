afc0b52e2267ca8943c976c7cd9e5ecc6523ec88
Vasil Krastev <vasilkrastev90@yahoo.com>
2016-08-21 16:43:39 +0300

    simple node server using the express framework

--- WebContent/nodeServer.js
+++ WebContent/nodeServer.js
@@ -1,19 +1,33 @@
-//Lets require/import the HTTP module
-var http = require('http');
+const path = require('path') 
+const express = require('express')  
+const exphbs = require('express-handlebars')
+const app = express()
 
-//Lets define a port we want to listen to
-const PORT=8080; 
+app.use(express.static('.'));
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')  
+app.set('views', path.join(__dirname, 'views')) 
 
-//Create a server
-var server = http.createServer(handleRequest);
+app.get('/', (request, response) => {  
+ response.render('home', {
+    name: 'John'
+  })
+})
 
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
+app.get('/error', (request, response) => {  
+ throw new Error('oops')
+})
+
+app.use((err, request, response, next) => {  
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+app.listen(3000) 
+console.log(`server is listening on 3000`)
