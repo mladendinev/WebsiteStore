d913cf13c17f9c38a9eafb663c96b50331101159
forchose <mladen.dinev@student.manchester.ac.uk>
2016-09-05 00:12:38 +0300

    Removed footer

--- webstore-clothes/imports/ui/pages/shopping-template.js
+++ webstore-clothes/imports/ui/pages/shopping-template.js
@@ -8,10 +8,11 @@ import {Inventory}  from '../../api/products.js';
 
 Template.shoppingTemplate.helpers({
     shopping(){
-    return Inventory.find({});
+    return Inventory.find({'type':'shirts'});
   }
 });
 
 Template.shoppingTemplate.onRendered(function(){
    Session.set("DocumentTitle","Shopping Page");
+    $("body").addClass("body-shopping");
 });
