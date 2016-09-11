93ff411d88f8cf42afb8c90c0ac0a9ccaaeae99d
forchose <mladen.dinev@student.manchester.ac.uk>
2016-09-11 19:05:13 +0300

    mailgun SMTP config

---
+++ webstore-clothes/server/methods/sendEmail.js
@@ -0,0 +1,13 @@
+SSR.compileTemplate('htmlEmail',Assets.getText('emailTemplate.html'));
+
+var emailData= {
+  name: "Mladen Dinev",
+  collection_date: "10th November",
+};
+
+Email.send({
+  to: "mladenddinev@gmail.com",
+  from: "f0rch0s3@gmail.com",
+  subject: "Example Email",
+  html: SSR.render('htmlEmail', emailData),
+});
