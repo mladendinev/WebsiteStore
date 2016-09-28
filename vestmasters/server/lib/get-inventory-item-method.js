a9b0948bde4f60a659670c4f0eb183e62cb7db54
Vasil Krastev <vasilkrastev90@yahoo.com>
2016-09-28 16:02:40 +0300

    deleted obsolete file

--- vestmasters/server/lib/get-inventory-item-method.js
+++
@@ -1,8 +0,0 @@
-import {Inventory} from '../../imports/api/products.js';
-
-Meteor.methods({
-   getSingleItem: function (id) {
-     check(id,String);
-     return Inventory.findOne({"_id" : new Meteor.Collection.ObjectID(id)});
-   }
-});
