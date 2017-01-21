c24f52c8fff0ed8338ca7e68af04cd38b43ea742
forchose <mladen.dinev@student.manchester.ac.uk>
2017-01-21 13:53:51 +0000

    temp

--- vestmasters/server/main.js
+++ vestmasters/server/main.js
@@ -9,6 +9,23 @@ import './publications.js'
 
 var gateway;
 
+function sendConfirmationEmail(clientEmail,subjectEmail, emailData){
+     check(clientEmail,String);
+     check(subjectEmail,String);
+     check(emailData,Object); //TODO IMPORTANT check argument for security reasons
+
+
+//    var html=Blaze.toHTMLWithData(Template.shareEmailContent,emailData);
+     SSR.compileTemplate('htmlEmail',Assets.getText('emailTemplate.html'));
+
+     Email.send({
+       to: clientEmail,
+       from: "clients@vestmasters.com",
+       subject: subjectEmail,
+       html: SSR.render('htmlEmail', emailData),
+//      html: html,
+      });
+}
 
 
 Meteor.startup(() => {
@@ -31,62 +48,40 @@ Meteor.startup(() => {
 
 
 Meteor.methods({
-
-  sendConfirmationEmail: function(clientEmail,subjectEmail, emailData){
-    check(clientEmail,String);
-    check(subjectEmail,String);
-    check(emailData,Object); //TODO IMPORTANT check argument for security reasons
-
-
-//    var html=Blaze.toHTMLWithData(Template.shareEmailContent,emailData);
-    SSR.compileTemplate('htmlEmail',Assets.getText('emailTemplate.html'));
-
-    Email.send({
-      to: clientEmail,
-      from: "clients@vestmasters.com",
-      subject: subjectEmail,
-      html: SSR.render('htmlEmail', emailData),
-//      html: html,
-    });
-
-    console.log('confirmation email sent');
-  },
-
-    subscribeEmail: function(subscriber,subjectEmail){
+   subscribeEmail: function(subscriber,subjectEmail){
       check(subscriber,String);
       check(subjectEmail,String);
 
       Email.send({
-        to:"clients@2ffashiongroup.com",
-        from: subscriber,
-        subject: subjectEmail,
-        text: "User has subscribed"
+         to:"clients@2ffashiongroup.com",
+         from: subscriber,
+         subject: subjectEmail,
+         text: "User has subscribed"
       });
     },
 
 
    getClientToken: function () {
-     var generateToken = Meteor.wrapAsync(gateway.clientToken.generate, gateway.clientToken);
-     var response = generateToken({});
-     return response.clientToken;
+      var generateToken = Meteor.wrapAsync(gateway.clientToken.generate, gateway.clientToken);
+      var response = generateToken({});
+      return response.clientToken;
    },
 
    createTransaction: function(nonceFromTheClient,basketId,deliveryDetails,secret) {
-    var gatewayTransactionSync = Meteor.wrapAsync(gateway.transaction.sale,gateway.transaction); 
-       check(nonceFromTheClient,String);
-       check(basketId,String); //TODO Add proper check for the items, otherwise a security risk
-       check(secret,String);
-       check(deliveryDetails,Object); //TODO Add proper check for the delivery, otherwise a security risk
+      var gatewayTransactionSync = Meteor.wrapAsync(gateway.transaction.sale,gateway.transaction);
+      check(nonceFromTheClient,String);
+      check(basketId,String); //TODO Add proper check for the items, otherwise a security risk
+      check(secret,String);
+      check(deliveryDetails,Object); //TODO Add proper check for the delivery, otherwise a security risk
        
       now = new Date();
       //Make sure the cart is still active and set to 'pending'. Also
       // fetch the cart details so we can calculate the checkout price
-      var result = Baskets.update(
-        {'_id': basketId, 'secret' : secret,'status': 'active',},
-        update={$set: { 'status': 'pending','lastModified': now } } )
-       if (result ===0) {
-        throw new Meteor.Error("INACTIVE CART", "Your cart has expired");
-       }
+      var result = Baskets.update({'_id': basketId, 'secret' : secret,'status': 'active',},
+      update={$set: { 'status': 'pending','lastModified': now } } )
+      if (result ===0) {
+         throw new Meteor.Error("INACTIVE CART", "Your cart has expired");
+      }
 
       var basket = Baskets.findOne({"_id" : basketId,'secret' : secret});
 
@@ -98,46 +93,51 @@ Meteor.methods({
       var queryArray = [];
       basket.itemsDetails.forEach(function(item){
        
-       if((typeof itemsDict[item.oid+"quantity"] === "undefined") || itemsDict[item.oid+"quantity"] === null){
-        itemsDict[item.oid+"quantity"] = 0;
-       }
-       queryArray.push({"_id": new Meteor.Collection.ObjectID(item.oid)}); 
-       item.initials.forEach(function(initial){
-        var quantityCounter = item["quantity" + initial];
-        itemsDict[item.oid+"quantity"] =itemsDict[item.oid+"quantity"] + quantityCounter;
-        })
-     })
-     items = Inventory.find({$or: queryArray}).fetch();
-     var deliveryPrice = (Countries.findOne({'country': deliveryDetails.country_delivery})).price;
-     console.log(deliveryPrice);
-     var totalAmountToPay = calculatePriceCallServer(items,itemsDict) + deliveryPrice;
-     try {
-     var result = gatewayTransactionSync({amount: totalAmountToPay,
+         if((typeof itemsDict[item.oid+"quantity"] === "undefined") || itemsDict[item.oid+"quantity"] === null){
+            itemsDict[item.oid+"quantity"] = 0;
+         }
+         queryArray.push({"_id": new Meteor.Collection.ObjectID(item.oid)});
+         item.initials.forEach(function(initial){
+           var quantityCounter = item["quantity" + initial];
+           itemsDict[item.oid+"quantity"] =itemsDict[item.oid+"quantity"] + quantityCounter;
+         })
+       })
+      items = Inventory.find({$or: queryArray}).fetch();
+      var deliveryPrice = (Countries.findOne({'country': deliveryDetails.country_delivery})).price;
+      var totalAmountToPay = calculatePriceCallServer(items,itemsDict) + deliveryPrice;
+      try {
+         var result = gatewayTransactionSync({amount: totalAmountToPay,
                                          paymentMethodNonce: nonceFromTheClient,
                                          options: {
                                          submitForSettlement: true
                                          }
                                          });
-      if(!result.success){
-         Baskets.update(
-        {'_id': basketId},
-        update={$set: { 'status': 'active'}});
-       throw new Meteor.Error("TRANSACTION_FAULURE","Problem with transaction");
+         if(!result.success){
+             Baskets.update({'_id': basketId},update={$set: { 'status': 'active'}});
+             throw new Meteor.Error("TRANSACTION_FAULURE","Problem with transaction");
+         }
       }
-     } catch(transactionError){
-         Baskets.update(
-        {'_id': basketId},
-        update={$set: { 'status': 'active'}});
-       throw new Meteor.Error("TRANSACTION_FAULURE","Problem with transaction");
-     }
+      catch(transactionError){
+          Baskets.update({'_id': basketId},update={$set: { 'status': 'active'}});
+          throw new Meteor.Error("TRANSACTION_FAULURE","Problem with transaction");
+      }
      
-     Meteor.defer(function(){
-      Baskets.remove({'_id': basketId });
-        Inventory.update({'carted.cartId': basketId},
-              {$pull: {'carted' :{'cartId': basketId}}},
-              {multi: true})
-     })
-     var orderId = Orders.insert({"items" : basket,
+      Meteor.defer(function(){
+         emailData = {'order_id': result.transactionId,
+                     'products': basket,
+                     "deliveryInfo":deliveryDetails,
+                      "amount" : result.transaction.amount,
+                      "currency" :result.transaction.currencyIsoCode};
+         console.log(basket);
+         console.log(deliveryDetails);
+         sendConfirmationEmail(deliveryDetails.email_addr, "Confirmation Email (Vest Masters)",emailData);
+
+          Baskets.remove({'_id': basketId });
+          Inventory.update({'carted.cartId': basketId},
+                                    {$pull: {'carted' :{'cartId': basketId}}},
+                                    {multi: true});
+       });
+       var orderId = Orders.insert({"items" : basket,
                                     "transactionId" : result.transaction.id,
                                     "amount" : result.transaction.amount,
                                     "currency" : result.transaction.currencyIsoCode,
